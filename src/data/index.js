a951d5018f95f5fb89d1c2fd0af53e8ab992c655
jawad <jawadefaj006@gmail.com>
2020-12-01 00:53:06 -0800

    vue, vue router, vuetify added

---
+++ src/data/index.js
@@ -0,0 +1,30 @@
+const profile = {
+    name: "Abdul Jawad",
+    phone: "123234",
+    publications: [
+        {
+            name: "Paper 1",
+            url: "link"
+        },
+        {
+            name: "Paper 2",
+            url: "link"
+        }
+    ],
+    education: [
+        {
+            name: "SSC",
+            passingYear: 2009,
+
+        },
+        {
+            name: "HSC",
+            passingYear: 2011,
+            
+        }
+    ]
+}
+
+
+
+export default profile;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-main>
+      <v-container>
+        {{name}}
+
+        <!-- Education Begins -->
+        <v-card flat>
+          <v-card-title>Education</v-card-title>
+          <v-card-text>
+            <v-list-item v-for="(degree, degreeIndex) in education" :key=degreeIndex>
+              {{degree.name}}
+            </v-list-item>
+          </v-card-text>
+        </v-card>
+        <!-- Education Ends -->
+
+        <!-- Publications Begins -->
+        <v-card>
+          <v-card-title>Publications</v-card-title>
+          <v-card-text>
+            <v-list-item v-for="(publication, publicationIndex) in publications" :key=publicationIndex>
+              {{publication.name}}
+            </v-list-item>
+          </v-card-text>
+        </v-card>
+        <!-- Publications Ends -->
+
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import profile from '@/data'
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+  },
+  data: function(){
+    return profile;
   }
 };
 </script>
